d1d05a8193bbacafbab4cc2a5e5fddcdda03340d
cemilsucu9 <cemilsucu9@gmail.com>
2021-07-07 18:08:33 +0300

    Movie service edited.

--- NetflixClone/src/app/main/services/movie.service.ts
+++ NetflixClone/src/app/main/services/movie.service.ts
@@ -1,9 +1,28 @@
+import { Movies } from './../models/movie.model';
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+
+const enum endpoint {
+  lastest='/movie/lastest',
+  now_playing='/movie/now_playing',
+  popular='/movie/popular',
+  top_rated='/movie/top_rated',
+  upcoming='/movie/upcoming',
+  trending='/trending/all/week',
+  originals='/discover/tv'
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getMovies() : Observable<Movies> {
+    return this.http.get<Movies>(`${environment.apiUrl}`)
+  }
 }
