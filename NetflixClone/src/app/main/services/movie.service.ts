e77582bf9220d10edd05bfcca4553792421e4316
cemilsucu9 <cemilsucu9@gmail.com>
2021-07-08 16:45:56 +0300

    Service completed.

--- NetflixClone/src/app/main/services/movie.service.ts
+++ NetflixClone/src/app/main/services/movie.service.ts
@@ -1,28 +1,76 @@
-import { Movies } from './../models/movie.model';
-import { HttpClient} from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { Movies } from '../models/movie.model';
 import { environment } from 'src/environments/environment';
 
-
 const enum endpoint {
-  lastest='/movie/lastest',
-  now_playing='/movie/now_playing',
-  popular='/movie/popular',
-  top_rated='/movie/top_rated',
-  upcoming='/movie/upcoming',
-  trending='/trending/all/week',
-  originals='/discover/tv'
+  latest = '/movie/latest',
+  now_playing = '/movie/now_playing',
+  popular = '/movie/popular',
+  top_rated = '/movie/top_rated',
+  upcoming = '/movie/upcoming',
+  trending = '/trending/all/week',
+  originals = '/discover/tv'
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private URL = 'https://api.themoviedb.org/3';
+  // tslint:disable-next-line:variable-name
+  private api_key = environment.api;
 
-  constructor(private http: HttpClient) {}
-
-  getMovies() : Observable<Movies> {
-    return this.http.get<Movies>(`${environment.apiUrl}`)
+  constructor(private http: HttpClient) {
   }
+
+  getLatestMovie(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.latest}`, {
+      params: {
+        api_key: this.api_key
+      }
+    });
+  }
+
+  getNowPlaying(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.now_playing}`, {
+      params: {
+        api_key: this.api_key
+      }
+    });
+  }
+
+  getOriginals(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.originals}`, {
+      params: {
+        api_key: this.api_key
+      }
+    });
+  }
+
+  getPopularMovies(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.popular}`, {
+      params: {
+        api_key: this.api_key
+      }
+    });
+  }
+
+  getTopRated(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.top_rated}`, {
+      params: {
+        api_key: this.api_key
+      }
+    });
+  }
+
+  getTrending(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.trending}`, {
+      params: {
+        api_key: this.api_key
+      }
+    });
+  }
+
 }
